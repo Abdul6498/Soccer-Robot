3cb25f4a9d2735d39f0f40f4cef21852daa5083a
Abdul6498 <73305650+Abdul6498@users.noreply.github.com>
2021-10-06 23:02:27 +0200

    Add files via upload

---
+++ Keyboard_rece_client/Keyboard_rece_client.ino
@@ -0,0 +1,148 @@
+//robot side
+//2.receiving part
+#include <SPI.h>
+#include <Mirf.h>
+#include <nRF24L01.h>
+#include <MirfHardwareSpiDriver.h>
+
+int motorpinb1 = 4;
+int motorpinb2 = 5;
+int motorpinf1 = 2;
+int motorpinf2 = 3;
+int solenoidpin = 9;
+int kickpin = 6;
+void setup(){
+   pinMode(motorpinb1,OUTPUT);
+   pinMode(motorpinb2,OUTPUT);
+   pinMode(motorpinf1,OUTPUT);
+   pinMode(motorpinf2,OUTPUT);
+   pinMode(kickpin,OUTPUT);
+   
+   digitalWrite(motorpinb1,LOW);
+   digitalWrite(motorpinb2,LOW);
+   digitalWrite(motorpinf1,LOW);
+   digitalWrite(motorpinf2,LOW);
+   digitalWrite(kickpin,LOW);
+   
+   pinMode(solenoidpin ,OUTPUT);
+   analogWrite(solenoidpin , 125);
+  Serial.begin(9600);
+ 
+  Mirf.spi = &MirfHardwareSpi;
+  Mirf.init();
+  Mirf.setRADDR((byte *)"serv1"); 
+  Mirf.payload = sizeof(unsigned long); 
+  Mirf.config();
+}
+
+void loop(){
+  byte data[Mirf.payload];   
+  if(!Mirf.isSending() && Mirf.dataReady()){
+    Serial.println("Got packet");
+    Mirf.getData(data);
+   // char a = Mirf.getData(data);
+   Serial.print("First value:");
+    Serial.println(byte (data[0]));
+  }
+  
+  if(data[0] <= 58)
+  {
+  move_right();
+  Serial.println("MOVE RIGHT");
+  }
+  if(data[0] >= 78)
+  {
+    if(data[0] <= 124){
+      move_left();
+  Serial.println("MOVE LEFT");
+    }
+  }
+
+  
+ //delay(300);   
+
+  
+  //  byte data[Mirf.payload];   
+
+  if(data[0] >= 182)
+  {
+    if(data[0] <= 254)
+    {
+    move_forward();
+  Serial.println("MOVE FORWARD");
+    }
+  }
+  if(data[0] <= 168)
+  {
+    if(data[0] >= 140)
+    {
+      move_back();
+  Serial.println("MOVE BACK");
+    }
+  }
+  if(data[0] > 168)
+  {
+    if(data[0] < 182)
+    {
+    //stop forward back but move right
+    
+   Serial.println("stop F & B");
+   
+    }
+  }
+// STOP THE CAR
+
+
+  
+   // delay(300);
+  
+
+    if(data[0] == 255)
+    {
+     kick_ball();
+    Serial.println("KICK THE BALL");
+    digitalWrite(kickpin , HIGH);
+    delay(100);
+    }
+    else
+    {
+     digitalWrite(kickpin , LOW);
+    }
+  //  delay(300);
+
+  delay(50);
+}
+void kick_ball()
+{
+
+}
+void move_left()
+{
+   digitalWrite(motorpinb1,LOW);
+   digitalWrite(motorpinb2,LOW);
+   digitalWrite(motorpinf1,HIGH);
+   digitalWrite(motorpinf2,LOW);
+}
+void move_right()
+{
+   digitalWrite(motorpinb1,HIGH);
+   digitalWrite(motorpinb2,LOW);
+   digitalWrite(motorpinf1,LOW);
+   digitalWrite(motorpinf2,LOW);
+}
+
+void move_forward()
+{
+   digitalWrite(motorpinb1, HIGH);
+   digitalWrite(motorpinb2,LOW);
+   digitalWrite(motorpinf1,HIGH);
+   digitalWrite(motorpinf2,LOW);
+}
+void move_back()
+{
+   digitalWrite(motorpinb1,LOW);
+   digitalWrite(motorpinb2,HIGH);
+   digitalWrite(motorpinf1,LOW);
+   digitalWrite(motorpinf2,HIGH);
+}
+
---
+++ Keyboard_send_server/Keyboard_send_server.ino
@@ -0,0 +1,80 @@
+//joystick side
+//1.sending part
+#include <SPI.h>
+#include <Mirf.h>
+#include <nRF24L01.h>
+#include <MirfHardwareSpiDriver.h>
+
+//int ledPin = 13;   // select the pin for the LED
+byte xval;
+byte yval;
+byte val = 0;
+
+//byte time;
+int xpin = A0;
+int ypin = A1;
+int kick = 3;
+
+void setup(){
+ // pinMode(ledPin,OUTPUT);    // declare the LED's pin as output
+  Serial.begin(9600);  
+    Mirf.csnPin = 9;
+    Mirf.cePin = 10;
+ 
+  
+  Mirf.spi = &MirfHardwareSpi;
+  Mirf.init();
+  Mirf.setRADDR((byte *)"clie1");
+  Mirf.payload = sizeof(unsigned long);
+  Mirf.config();
+ pinMode(xpin , INPUT);
+ pinMode(ypin , INPUT);
+ pinMode(kick , INPUT);
+ pinMode(kick , LOW);
+
+ 
+}
+void loop(){
+ /*
+  if (Serial.available()){
+    val = Serial.read();
+
+  }*/
+  xval = analogRead(xpin)/8.25; //data[0]
+ delay(10); 
+  yval = analogRead(ypin)/8.25 + 125; //data[1]
+delay(10);
+ // delay(300);
+
+  Serial.println(xval);
+  Serial.println();
+  Serial.println(yval);
+  
+  //delay(300);
+   // byte time;
+ //   time = val;
+
+    Mirf.setTADDR((byte *)"serv1");
+    Mirf.send((byte *)&xval);  
+    while(Mirf.isSending()){
+    }
+
+    Mirf.setTADDR((byte *)"serv1");
+    Mirf.send((byte *)&yval);  
+    while(Mirf.isSending()){
+    }
+    
+    if(digitalRead(kick) == HIGH)
+    {
+      val = 255;  
+    Mirf.setTADDR((byte *)"serv1");
+    Mirf.send((byte *)&val);  
+    while(Mirf.isSending()){
+    }        
+    }
+    else
+    {
+    val = 0;
+    }
+  
+}
